8171e06b550084bcacfc8bf82f1d9eb060b1ca2e
KomalKrishna12 <komalkrishna016@gmail.com>
2023-05-19 11:52:28 +0530

    iniial commit

---
+++ Array2D.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Array2D {
+
+    public static void display(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = { { 1, 7 }, { 9, 8 }, { 6, 3 } };
+
+        display(arr);
+
+        for (int[] a : arr) {
+            Arrays.sort(a);
+        }
+
+        display(arr);
+
+    }
+}
